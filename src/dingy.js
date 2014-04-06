171303d58990535955e7e136d31781eace0c732b
Will Jones <will@sacharissa.co.uk>
2014-04-06 18:42:53 +0100

    Add support for singletons and UMD.

--- src/dingy.js
+++ src/dingy.js
@@ -1,60 +1,100 @@
-var Dingy = function() {
-  var self = this;
+(function(root, module) {
+  if (typeof define === 'function' && define.amd) {
+    define([], module);
+  }
+  else if (typeof exports === 'object') {
+    module.exports = module();
+  }
+  else {
+    root.Dingy = module();
+  }
+})(this, function() {
+  var Dingy = function(registrations) {
+    var self = this;
 
-  var registrations = {};
+    self.resolve = function(name) {
+      var registration = registrations[name];
 
-  self.registerInstance = function(name, instance) {
-    registrations[name] = instance;
-  };
-
-  self.registerFunction = function(name, f) {
-    var registration = function() { return f.apply(null, arguments); };
-    registration.dependencies = getDependencies(f);
-
-    registrations[name] = registration;
-  };
-
-  self.registerClass = function(name, klass) {
-    var registration = getConstructor(klass);
-    registration.dependencies = getDependencies(klass);
-
-    registrations[name] = registration;
-  };
-
-  var getConstructor = function(klass) {
-    function Proxy(args) {
-      return klass.apply(this, args);
-    }
-
-    Proxy.prototype = klass.prototype;
-
-    return function(args) {
-      return new Proxy(args);
+      return typeof registration === 'function' ?
+        registration(registration.dependencies.map(self.resolve)) :
+        registration;
     };
   };
 
-  var getDependencies = function(f) {
-    return getDependenciesFromArguments(f);
-  };
+  Dingy.Builder = function() {
+    var self = this,
+        registrations = {};
 
-  var getDependenciesFromArguments = (function() {
-    var ARGUMENTS_PATTERN = /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
-        trim = function(s) { return s.trim(); };
-
-    return function(f) {
-      var functionDefinition = f.toString(),
-          argsDefinition = functionDefinition.match(ARGUMENTS_PATTERN)[1],
-          args = argsDefinition ? argsDefinition.split(',').map(trim) : [];
-
-      return args;
+    self.inflate = function() {
+      return new Dingy(registrations);
     };
-  })();
 
-  self.resolve = function(name) {
-    var registration = registrations[name];
+    self.registerInstance = function(name, instance) {
+      registrations[name] = instance;
+    };
 
-    return typeof registration === 'function' ?
-      registration(registration.dependencies.map(self.resolve)) :
-      registration;
+    self.registerFunction = function(name, f) {
+      var g = function(args) { return f.apply(null, args); };
+      g.dependencies = getDependencies(f);
+
+      registrations[name] = g;
+      return new Dingy.Builder.Registration(registrations, name, g);
+    };
+
+    self.registerClass = function(name, klass) {
+      var constructor = getConstructor(klass);
+      constructor.dependencies = getDependencies(klass);
+
+      registrations[name] = constructor;
+      return new Dingy.Builder.Registration(
+        registrations, name, constructor);
+    };
+
+    var getConstructor = function(klass) {
+      function Proxy(args) {
+        return klass.apply(this, args);
+      }
+
+      Proxy.prototype = klass.prototype;
+
+      return function(args) {
+        return new Proxy(args);
+      };
+    };
+
+    var getDependencies = function(f) {
+      return getDependenciesFromArguments(f);
+    };
+
+    var getDependenciesFromArguments = (function() {
+      var ARGUMENTS_PATTERN = /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
+          trim = function(s) { return s.trim(); };
+
+      return function(f) {
+        var functionDefinition = f.toString(),
+            argsDefinition = functionDefinition.match(ARGUMENTS_PATTERN)[1],
+            args = argsDefinition ? argsDefinition.split(',').map(trim) : [];
+
+        return args;
+      };
+    })();
   };
-};
+
+  Dingy.Builder.Registration = function(registrations, name, implementation) {
+    var self = this;
+
+    self.asSingleton = function() {
+      var memoizedImplementation = function(args) {
+        return registrations[name] = implementation(args);
+      };
+
+      memoizedImplementation.dependencies =
+        implementation.dependencies;
+
+      registrations[name] = memoizedImplementation;
+      return self;
+    };
+  };
+
+  return Dingy;
+});
